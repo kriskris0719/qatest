05d7448d45e6f684fabf700e0b26c3ba7faa2b1c
Khrystyna Zhelo <kriart0719@gmail.com>
2022-09-20 11:31:10 +0300

    Add to Chapter5

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,27 +6,32 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
     private WebDriver driver;
 
-    public HomePage(WebDriver driver){
+    public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public DropdownPage clickDropDown(){
+    public DropdownPage clickDropDown() {
         clickLink("Dropdown");
         return new DropdownPage(driver);
     }
 
-    public LoginPage clickFormAuthentication(){
+    public LoginPage clickFormAuthentication() {
         clickLink("Form Authentication");
         return new LoginPage(driver);
     }
 
-    public ForgorPasswordPage clickForgotPassword(){
+    public ForgorPasswordPage clickForgotPassword() {
         clickLink("Forgot Password");
         return new ForgorPasswordPage(driver);
     }
 
-    private void clickLink(String linkText){
-        driver. findElement(By.linkText(linkText)).click();
+    public HoversPage clickHovers(){
+        clickLink("Hovers");
+        return new HoversPage(driver);
+    }
+
+    private void clickLink(String linkText) {
+        driver.findElement(By.linkText(linkText)).click();
 
     }
 }
--- src/test/java/dropdown/DropdownTests.java
+++ src/test/java/dropdown/DropdownTests.java
@@ -2,6 +2,9 @@ package dropdown;
 
 import base.BaseTests;
 import org.testng.annotations.Test;
+import pages.DropdownPage;
+
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -10,10 +13,10 @@ public class DropdownTests extends BaseTests {
 
     @Test
     public void testSelectOption(){
-        var dropDownPage = homePage.clickDropDown();
+        DropdownPage dropDownPage = homePage.clickDropDown();
         String option = "Option 1";
         dropDownPage.selectFromDropDown(option);
-        var selectedOptions = dropDownPage.getSelectedOptions();
+        List<String> selectedOptions = dropDownPage.getSelectedOptions();
         assertEquals(selectedOptions.size(), 1, "Incorrect number of selections");
         assertTrue(selectedOptions.contains(option), "Option not selected");
     }
